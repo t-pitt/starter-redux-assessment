600e2e8642240617bdaccec51df6c433480d236e
Thomas <tpitt@indeed.com>
2023-12-20 10:46:03 -0800

    Remove unused selectAllPhotos.

--- src/features/photos/list/index.js
+++ src/features/photos/list/index.js
@@ -2,7 +2,6 @@ import { useSelector, useDispatch } from 'react-redux';
 import {
   // Task 7: Import the `removePhoto()` action creator from the photos slice
   removePhoto,
-  selectAllPhotos,
   // Task 13: Import the `selectFilteredPhotos()` selector from the photos slice
   selectFilteredPhotos
 } from '../photos.slice';
